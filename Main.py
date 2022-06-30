a4671258d7ddff517b8244e254f720f2fc8bc37e
Aayush-Gangwar <101112022+Aayush-Gangwar@users.noreply.github.com>
2022-07-01 02:57:41 +0530

    Initial commit

---
+++ Main.py
@@ -0,0 +1,25 @@
+# importing neccessary python libraries used for system.
+from win32com.client import Dispatch
+import pyttsx3
+import datetime
+from bs4 import BeautifulSoup
+import speech_recognition as sr
+import pyaudio
+import numpy as np
+import wikipedia
+import requests
+import webbrowser
+import sports
+import os
+import pywhatkit
+import smtplib
+import cv2
+import wolframalpha
+import sys
+import winsound
+import pyautogui
+from email.mime.text import MIMEText
+from pycricbuzz import Cricbuzz
+import speedtest
+import random
+

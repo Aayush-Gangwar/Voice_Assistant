9e9179ec67cdd896ec03629b44bcc6098bf08831
Aayush-Gangwar <101112022+Aayush-Gangwar@users.noreply.github.com>
2022-07-02 22:47:53 +0530

    speak and query accept

--- Main.py
+++ Main.py
@@ -23,3 +23,30 @@ from pycricbuzz import Cricbuzz
 import speedtest
 import random
 
+# setting up the voices and engine to accept commands for system from user.
+engine=pyttsx3.init("sapi5")
+voices=engine.getProperty("voices")
+engine.setProperty("voice", voices[1].id)
+engine.setProperty("rate",130)
+
+def speak(text):   # speak function/module of system..help in converting text to voice.
+    engine.say(text)
+    engine.runAndWait()
+
+def acceptcommands():   # module for accepting voice input from user and recognizes with google_recognize. and converting into text string.
+    c = sr.Recognizer()
+    with sr.Microphone() as source:
+        speak("Listening---")
+        c.pause_threshold=0.5
+        c.energy_threshold= 100
+        audio=c.listen(source)
+
+    try:
+        print("recognizing")
+        query=c.recognize_google(audio,language="en-in")
+        speak(query)
+        print(f"user said: {query}")
+    except Exception as error:
+        # speak("can't recognize..please speak again...")
+        return "NONE"
+    return query

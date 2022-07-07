c8b32a7ee38e431c2b5f8591d1767d0568f67d72
Aayush-Gangwar <101112022+Aayush-Gangwar@users.noreply.github.com>
2022-07-07 09:53:50 +0530

    facelock module

--- Main.py
+++ Main.py
@@ -134,3 +134,36 @@ def face_lock():    # security module ...unlock the system by recognizes the cor
                         cv2.imwrite(img_name, frame)
                         img_counter += 1
                         c+=1
+   # cam.release()
+    cv2.destroyAllWindows()
+    # function for comapring new face id and base user face id.
+    base = cv2.imread("opencv_frame_base.png")
+    test = cv2.imread("opencv_frame_0.png")
+    hsv_base = cv2.cvtColor(base, cv2.COLOR_BGR2HSV)
+    hsv_test = cv2.cvtColor(test, cv2.COLOR_BGR2HSV)
+
+    h_bins = 50
+    s_bins = 60
+    histSize = [h_bins, s_bins]
+    h_ranges = [0, 180]
+    s_ranges = [0, 256]
+    ranges = h_ranges + s_ranges
+    channels = [0, 1]
+
+    hist_base = cv2.calcHist([hsv_base], channels, None, histSize, ranges, accumulate=False)
+    cv2.normalize(hist_base, hist_base, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)
+
+    hist_test = cv2.calcHist([hsv_test], channels, None, histSize, ranges, accumulate=False)
+    cv2.normalize(hist_test, hist_test, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)
+
+    compare_method = cv2.HISTCMP_CORREL
+
+    base_test = cv2.compareHist(hist_base, hist_test, compare_method)
+    if(base_test>=0.8):  # unlock only when >=80% of images are same.
+        access= True
+        speak("Access granted")
+        wish()
+        Task_execution()  # task execution will start from this function.
+    else:
+        speak("Access denied")
+    cv2.waitKey(0)
